83d9f0b631a0ef45813bb6ce386e0a011cd4a138
Letciasiqueira <leticiasiqueira1605@gmail.com>
2026-09-01 22:55:37 -0300

    Finzalizando pthreads

--- main.c
+++ main.c
@@ -52,7 +52,7 @@ int interacoes(int MAX_interacoes, double pixelImaginario, double pixelreal){
 }
 
 long ler_entrada(char *argumento, char *nome){
-    FILE *Erros_ler_entrada = fopen("erros.txt", "a");
+    FILE *Erros_ler_entrada = fopen("erros.txt", "w");
 
     if (Erros_ler_entrada == NULL){
         exit(1);
@@ -75,7 +75,7 @@ double saida_Serial(int numero_largura, int numero_altura, int numero_MAXinterac
     
     struct timespec inicio, fim;
 
-    FILE *Erros_serial = fopen("erros.txt", "a");
+    FILE *Erros_serial = fopen("erros.txt", "w");
 
     if (Erros_serial == NULL){
         exit(1);
@@ -130,7 +130,7 @@ double saida_OpenMP(int numero_largura, int numero_altura, int numero_MAXinterac
     
     struct timespec inicio, fim;
 
-    FILE *Erros_OpenMP = fopen("erros.txt", "a");
+    FILE *Erros_OpenMP = fopen("erros.txt", "w");
 
     if (Erros_OpenMP == NULL){
         exit(1);
@@ -200,37 +200,83 @@ void *calcula_pixel_PThreads(void *struct_pthreads){
     return NULL;
 }
 
-double saida_Pthreads1(int numero_largura, int numero_altura, int numero_MAXinteracoes, int *pixel_da_imagem, int numero_threads){
+double saida_Pthreads1_malloc(int numero_largura, int numero_altura, int numero_MAXinteracoes, int *pixel_da_imagem, int numero_threads){
     struct timespec inicio, fim;
 
-    FILE *Erros_pthreads1 = fopen("erros.txt", "a");
+    FILE *Erros_pthreads1 = fopen("erros.txt", "w");
 
     if (Erros_pthreads1 == NULL){
         exit(1);
     }
 
-    pthread_t threads[numero_threads];
-    Estrutura_pthread dados_da_thread[numero_threads];
+    pthread_t *threads = malloc(numero_threads * sizeof(pthread_t));
+    Estrutura_pthread *dados_da_thread = malloc(numero_threads * sizeof(Estrutura_pthread));
+
+    if (threads == NULL || dados_da_thread == NULL){
+        fprintf(Erros_pthreads1, "Erro ao alocar memoria para as threads.\n");
+        free(threads);
+        free(dados_da_thread);
+        fclose(Erros_pthreads1);
+        exit(1);
+    }
 
     int linhas_por_thread = numero_altura / numero_threads;
     int resto = numero_altura % numero_threads;
 
     clock_gettime(CLOCK_MONOTONIC, &inicio);
+    int linha_atual = 0;
 
+    for (int t = 0; t < numero_threads; t++){
+
+        dados_da_thread[t].largura = numero_largura;
+
+        dados_da_thread[t].altura = numero_altura;
+
+        dados_da_thread[t].max_iteracoes = numero_MAXinteracoes;
+
+        dados_da_thread[t].pixel_da_imagem = pixel_da_imagem;
+
+        dados_da_thread[t].linha_inicial = linha_atual;
+
+        int linhas_desta_thread = linhas_por_thread;
+        if (t == numero_threads - 1){
+            linhas_desta_thread += resto;
+        }
+
+        dados_da_thread[t].linha_final = linha_atual + linhas_desta_thread;
+        linha_atual = dados_da_thread[t].linha_final;
+
+        int criando_threads = pthread_create(&threads[t], NULL, calcula_pixel_PThreads, &dados_da_thread[t]);
+
+        if (criando_threads != 0){
+            fprintf(Erros_pthreads1, "Erro ao criar thread %d\n", t);
+            free(threads);
+            free(dados_da_thread);
+            fclose(Erros_pthreads1);
+            exit(1);
+        }
+    }
+
+    for (int t = 0; t < numero_threads; t++){
+        pthread_join(threads[t], NULL);
+    }
     clock_gettime(CLOCK_MONOTONIC, &fim);
 
     double tempo_Pthreads1 = (fim.tv_sec - inicio.tv_sec) + (fim.tv_nsec - inicio.tv_nsec) / 1e9;
 
-    FILE *arquivoPThreads1 = fopen("mandelbrot_lmss4_openmp.pgm", "w");
+    FILE *arquivoPThreads1 = fopen("mandelbrot_lmss4_pthreads1.pgm", "w");
 
     if (arquivoPThreads1 == NULL){
-        fprintf(stderr, "Erro ao abrir o arquivo Pthreads 1.\n");
+        fprintf(Erros_pthreads1, "Erro ao abrir o arquivo Pthreads 1.\n");
+        free(threads);
+        free(dados_da_thread);
+        fclose(Erros_pthreads1);
         exit(1);
     }
 
     for (int i = 0; i < numero_altura; i++){
         for (int j = 0; j < numero_largura; j++){
-            
+
             if (j == 0){
 
                 fprintf(arquivoPThreads1, "%d", pixel_da_imagem[(i * numero_largura) + j]);
@@ -244,15 +290,64 @@ double saida_Pthreads1(int numero_largura, int numero_altura, int numero_MAXinte
             fprintf(arquivoPThreads1, "\n");
         }
     }
-    
+
     fclose(arquivoPThreads1);
+    fclose(Erros_pthreads1);
+    free(threads);
+    free(dados_da_thread);
 
     return tempo_Pthreads1;
 }
 
+double saida_pthreads2(int numero_largura, int numero_altura, int numero_MAXinteracoes, int *pixel_da_imagem, int numero_threads){
+
+    struct timespec inicio, fim;
+
+    FILE *Erros_pthreads2 = fopen("erros.txt", "w");
+
+    if (Erros_pthreads2 == NULL){
+        exit(1);
+    }
+
+
+
+
+
+    
+    double tempo_Pthreads2 = (fim.tv_sec - inicio.tv_sec) + (fim.tv_nsec - inicio.tv_nsec) / 1e9;
+
+    FILE *arquivoPThreads2 = fopen("mandelbrot_lmss4_pthreads2.pgm", "w");
+
+    if (arquivoPThreads2 == NULL){
+        fprintf(Erros_pthreads2, "Erro ao abrir o arquivo Pthreads 2.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < numero_altura; i++){
+        for (int j = 0; j < numero_largura; j++){
+            
+            if (j == 0){
+
+                fprintf(arquivoPThreads2, "%d", pixel_da_imagem[(i * numero_largura) + j]);
+            } else{
+
+                fprintf(arquivoPThreads2, " %d", pixel_da_imagem[(i * numero_largura) + j]);
+            }
+        }
+
+        if (i != numero_altura - 1){
+            fprintf(arquivoPThreads2, "\n");
+        }
+    }
+    
+    fclose(arquivoPThreads2);
+
+    return tempo_Pthreads2;
+}
+
 int main(int argc, char *argv[]){
     
-    FILE *Erros_main = fopen("erros.txt", "a");
+    FILE *Erros_main = fopen("erros.txt", "w");
 
     if (Erros_main == NULL){
         exit(1);
